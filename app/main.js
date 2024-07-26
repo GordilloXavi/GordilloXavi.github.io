8070ebb9bbda554153eef685c2e8113962e2adfa
xavi <xavigr00@gmail.com>
2024-07-26 12:55:57 +0200

    minecraft checkpoint commit

--- app/main.js
+++ app/main.js
@@ -1,31 +1,67 @@
 import * as THREE from 'three';
 import { Wireframe } from 'three/examples/jsm/Addons.js';
 import { color } from 'three/examples/jsm/nodes/Nodes.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { World } from './world';
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0x1040f0);
 document.body.appendChild( renderer.domElement );
 
 // Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(2, 2, 2);
+camera.position.set(-32, 15, -32);
 camera.lookAt(0, 0, 0);
 
 // Scene
 const scene = new THREE.Scene();
 
-// Draw cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// World
+const world = new World({width: 64, height: 32});
+world.generate();
+scene.add(world);
 
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Add a 3D GridHelper
+const gridHelper = new THREE.GridHelper(100, 100); // Size of the grid, divisions
+scene.add(gridHelper);
+
+// Lighting
+function setupLights() {
+    const light1 = new THREE.DirectionalLight();
+    light1.position.set(1,1,1);
+
+    scene.add(light1);
+
+    const light1_helper = new THREE.DirectionalLightHelper(light1, 0.1); // Size of the helper
+    scene.add(light1_helper);
+
+    const light2 = new THREE.DirectionalLight(0xff0000, 1); // red color, intensity 1
+    light2.position.set(-1, 1, -0.5);
+    scene.add(light2);
+
+    const light2_helper = new THREE.DirectionalLightHelper(light2, 0.1); // Size of the helper
+    scene.add(light2_helper);
+
+    const ambient_light = new THREE.AmbientLight();
+    ambient_light.intensity = 0.1;
+    scene.add(ambient_light);
+} 
 
 function animate() {
-    cube.rotateX(0.01);
-    cube.rotateY(0.01);
 	renderer.render( scene, camera );
 }
+
+setupLights();
 renderer.setAnimationLoop( animate );
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
