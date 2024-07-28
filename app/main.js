325ed2de967dee538b4279ebd4b89f50c7ba9bff
xavi <xavigr00@gmail.com>
2024-07-29 01:49:35 +0200

    [working] terrain chunk

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { Wireframe } from 'three/examples/jsm/Addons.js';
+import { FirstPersonControls, Wireframe } from 'three/examples/jsm/Addons.js';
 import { color } from 'three/examples/jsm/nodes/Nodes.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { createNoise2D } from 'simplex-noise';
@@ -14,34 +14,37 @@ document.body.appendChild( renderer.domElement );
 
 // Debugging tool
 const gui = new dat.GUI();
+const clock = new THREE.Clock();
 
 // Camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 camera.position.set(-32, 15, -32);
 camera.lookAt(0, 0, 0);
 
 // Scene
 const scene = new THREE.Scene();
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-
 // Add a 3D GridHelper
 //const gridHelper = new THREE.GridHelper(100, 100); // Size of the grid, divisions
 //scene.add(gridHelper);
 
+// Controls
+const controls = new FirstPersonControls( camera, renderer.domElement );
+controls.movementSpeed = 150;
+controls.lookSpeed = 0.1;
+
 // Terrain
-let terrain_width = 100;
-let terrain_height = 100;
+let terrain_width = 500;
+let terrain_height = 500;
 const terrain_geometry = new THREE.PlaneGeometry( terrain_width, terrain_height, terrain_width, terrain_height );
 terrain_geometry.rotateX(Math.PI / 2)
 
 const params = {
     scale: 0.04,
-    amplitude: 7
+    amplitude: 20
 };
-gui.add(params, 'scale', -1, 1).onChange(updateTerrain);
-gui.add(params, 'amplitude', 0, 10).onChange(updateTerrain);
+gui.add(params, 'scale', 0, 0.1).onChange(updateTerrain);
+gui.add(params, 'amplitude', 0, 50).onChange(updateTerrain);
 
 const noise2D = createNoise2D();
 
@@ -79,9 +82,6 @@ scene.add( terrain_mesh );
 
 const terrain_vertices = terrain_geometry.attributes.position.array
 
-
-
-
 // Lighting
 function setupLights() {
     const light1 = new THREE.DirectionalLight();
@@ -97,7 +97,9 @@ function setupLights() {
     scene.add(ambient_light);
 } 
 
+
 function animate() {
+    controls.update( clock.getDelta() );
 	renderer.render( scene, camera );
 }
 
@@ -108,4 +110,5 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    controls.handleResize();
 });
